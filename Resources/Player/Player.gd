df14c770ca30b30b0e99b17332eb92c33b98368e
Southworth <southworth@southworth-LIFEBOOK-T732>
2020-05-13 15:44:44 -0400

    Completed lesson 6
    animation trees

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -7,11 +7,14 @@ const FRICTION = 500
 
 # Variables
 var velocity = Vector2.ZERO 
-var input_vector = Vector2.ZERO
+var input_vector = Vector2.ZERO 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print ("Hello World")
+	print ("Begin Game") 
 
 # note to self delta is 1/60th of a second.
 func _physics_process(delta):
@@ -20,40 +23,19 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
+	#Lesson 5 - Using AnimationTree
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		print(velocity)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = velocity.clamped(MAX_SPEED)
 	velocity = move_and_slide(velocity)
-	
-	# Lesson 1
-	# Basic Keyboard action control - no diagonals	
-	#if Input.is_action_pressed("ui_right"):
-	#	print ("Pressed Right Arrow Key")
-	#	velocity.x = 4
-	#elif Input.is_action_pressed("ui_left"):
-	#	print ("Pressed Left Arrow Key")
-	#	velocity.x = -4
-	#else:
-	#	print ("No Left or Right Key Pressed")
-	#	velocity.x = 0	
-		
-	#if Input.is_action_pressed("ui_up"):
-	#	print ("Pressed Up Arrow Key")
-	#	velocity.y = -4
-	#elif Input.is_action_pressed("ui_down"):
-	#	print ("Pressed Down Arrow Key")
-	#	velocity.y = 4
-	#else:
-	#	print ("No Up or Down Key Pressed")
-	#	velocity.y = 0
-		
-	#move_and_collide(velocity * delta)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+			
+			
